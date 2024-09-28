f69386d9355c54a03b4906000eade75aee3b4d97
Rohan Parmar <rohanrparmar987@gmail.com>
2024-09-29 00:39:53 +0530

    Initial Commit :rocket:

---
+++ README.md
@@ -0,0 +1,1 @@
+# Quick Helper - Chrome Extension
---
+++ assets/js/popup.js
@@ -0,0 +1,61 @@
+const apiUrl = '';
+
+const copySvg = `<svg aria-hidden="true" height="16" viewBox="0 0 16 16" version="1.1" width="16" data-view-component="true" class="octicon octicon-copy js-clipboard-copy-icon">
+<path d="M0 6.75C0 5.784.784 5 1.75 5h1.5a.75.75 0 0 1 0 1.5h-1.5a.25.25 0 0 0-.25.25v7.5c0 .138.112.25.25.25h7.5a.25.25 0 0 0 .25-.25v-1.5a.75.75 0 0 1 1.5 0v1.5A1.75 1.75 0 0 1 9.25 16h-7.5A1.75 1.75 0 0 1 0 14.25Z"></path><path d="M5 1.75C5 .784 5.784 0 6.75 0h7.5C15.216 0 16 .784 16 1.75v7.5A1.75 1.75 0 0 1 14.25 11h-7.5A1.75 1.75 0 0 1 5 9.25Zm1.75-.25a.25.25 0 0 0-.25.25v7.5c0 .138.112.25.25.25h7.5a.25.25 0 0 0 .25-.25v-7.5a.25.25 0 0 0-.25-.25Z"></path>
+</svg>`;
+
+
+document.addEventListener('DOMContentLoaded', () => {
+  fetch(apiUrl)
+  .then(response => response.json())
+  .then(data => {    
+    let main = document.querySelector('.main');
+    data.forEach(element => {
+
+      let wrapper = document.createElement('div');
+      wrapper.className = 'json-item';
+
+      let sectionName = document.createElement('span');
+      sectionName.className = 'section-name';
+      sectionName.innerText = element.section;
+      wrapper.appendChild(sectionName);
+
+      element.data.forEach((data) => {
+        let key = document.createElement('span');
+        key.className = 'section-key';
+        key.innerText = data.key;
+        wrapper.appendChild(key);
+
+        let valueWrap = document.createElement('div');
+        valueWrap.className = 'value-wrap';
+        
+        let value = document.createElement('span');
+        value.className = 'section-value';
+        value.innerText = data.value;
+        valueWrap.appendChild(value);
+
+        // Create a container for the copy SVG
+        let copyIcon = document.createElement('span');
+        copyIcon.className = 'copy-icon';  // Optional class for styling
+        copyIcon.innerHTML = copySvg;
+
+        // Add event listener to the copy button
+        copyIcon.addEventListener('click', () => {
+          navigator.clipboard.writeText(value.innerText).then(() => {
+            console.log('Copied to clipboard:', value.innerText);
+          }).catch(err => {
+            console.error('Failed to copy text: ', err);
+          });
+        });
+
+        valueWrap.appendChild(copyIcon);
+        wrapper.appendChild(valueWrap);
+      })
+      main.appendChild(wrapper);
+
+    });
+  })    
+  .catch(error => {
+    console.error('Error fetching JSON data:', error);
+  });
+})
