094bd697346d6d6d2d0c1db8d37e7d4a60182b77
Himanshu Ramavat <himanshuramavat07@gmail.com>
2024-10-05 17:20:20 +0530

    [TASK] Update workflow

---
+++ stylelint.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    extends: "stylelint-config-standard",
+    rules: {
+      "indentation": 2,
+      "string-quotes": "double",
+      "color-hex-case": "lower",
+      "block-no-empty": true,
+      "max-nesting-depth": 3
+    }
+  };
+  
