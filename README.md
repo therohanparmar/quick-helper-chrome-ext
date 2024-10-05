0346924d95fd0765a72bb4f230aae30f49d2fdd6
Himanshu Ramavat <77565273+himanshuramavat@users.noreply.github.com>
2024-10-05 20:47:03 +0530

    [TASK] Add JSONbin api and update documentation #7 (#9)

--- README.md
+++ README.md
@@ -1,26 +1,43 @@
 # Quick Helper - Chrome Extension
-Quickly access your required data and boost your development speed.
+Easily access the data you need to speed up your development work.
 
 ## Setup
 
 ### Download the Extension
 
-- Download the extension from [https://github.com/therohanparmar/quick-helper-chrome-ext/archive/refs/tags/1.0.0.zip](https://github.com/therohanparmar/quick-helper-chrome-ext/archive/refs/tags/1.0.0.zip)
+- Download the extension from this link: [Download Quick Helper](https://github.com/therohanparmar/quick-helper-chrome-ext/archive/refs/tags/1.0.0.zip).
 
 ### Load the Extension in Chrome:
 
-- Open Chrome and go to `chrome://extensions/`.
-- Enable `Developer mode` at the top right.
-- Click `Load unpacked` and select your extension folder.
+1. Open Chrome and go to `chrome://extensions/`.
+2. Turn on `Developer mode` in the top right corner.
+3. Click on `Load unpacked` and choose the folder where you saved the extension.
 
 ### Load the Extension in Firefox:
 
-- Open Firefox and go to `about:debugging#/runtime/this-firefox`.
-- Click `Load Temporary Add-on…` and select `manifest.json` from the extension.
+1. Open Firefox and go to `about:debugging#/runtime/this-firefox`.
+2. Click on `Load Temporary Add-on…` and select `manifest.json` from the extension folder.
 
 ### API Data
 
-The API response must be in this format
+We use some sample data from [JSONBin](https://jsonbin.io/) to get started.
+
+```javascript
+const apiUrl = 'https://api.jsonbin.io/v3/b/670101afacd3cb34a8919055';
+```
+
+If you want to use your own data, follow these steps:
+
+##### Steps:
+1. Go to [JSONBin](https://jsonbin.io/ "JSONBin") and sign up.
+2. Create a new bin with your JSON data.
+3. JSONBin will give you an API endpoint to access this data.
+4. Replace the `apiUrl` with your new API link.
+
+
+### API Response Format
+
+Make sure your API response looks like this:
 
 ```
 [
--- assets/js/popup.js
+++ assets/js/popup.js
@@ -1,4 +1,4 @@
-const apiUrl = '';
+const apiUrl = 'https://api.jsonbin.io/v3/b/670101afacd3cb34a8919055';
 
 const copySvg = `<svg aria-hidden="true" height="16" viewBox="0 0 16 16" version="1.1" width="16" data-view-component="true" class="octicon octicon-copy js-clipboard-copy-icon">
 <path d="M0 6.75C0 5.784.784 5 1.75 5h1.5a.75.75 0 0 1 0 1.5h-1.5a.25.25 0 0 0-.25.25v7.5c0 .138.112.25.25.25h7.5a.25.25 0 0 0 .25-.25v-1.5a.75.75 0 0 1 1.5 0v1.5A1.75 1.75 0 0 1 9.25 16h-7.5A1.75 1.75 0 0 1 0 14.25Z"></path><path d="M5 1.75C5 .784 5.784 0 6.75 0h7.5C15.216 0 16 .784 16 1.75v7.5A1.75 1.75 0 0 1 14.25 11h-7.5A1.75 1.75 0 0 1 5 9.25Zm1.75-.25a.25.25 0 0 0-.25.25v7.5c0 .138.112.25.25.25h7.5a.25.25 0 0 0 .25-.25v-7.5a.25.25 0 0 0-.25-.25Z"></path>
@@ -7,12 +7,11 @@ const copySvg = `<svg aria-hidden="true" height="16" viewBox="0 0 16 16" version
 document.addEventListener('DOMContentLoaded', () => {
   fetch(apiUrl)
     .then(response => response.json())
-    .then(data => {    
+    .then(data => {
       let main = document.querySelector('.main');
-      data.forEach(element => {
-        let wrapper = document.createElement('div');
-        wrapper.className = 'json-item';
-
+      let waitText = document.querySelector('.wait-text');
+      let wrapper = document.createElement('div'); // Initialize wrapper here
+      data.record.forEach(element => {
         let sectionName = document.createElement('span');
         sectionName.className = 'section-name';
         sectionName.innerText = element.section;
@@ -40,6 +39,7 @@ document.addEventListener('DOMContentLoaded', () => {
           // Add event listener to the copy button
           copyIcon.addEventListener('click', () => {
             navigator.clipboard.writeText(value.innerText).then(() => {
+              //TODO: add some code for success
             }).catch(err => {
               alert('Failed to copy text: ', err);
             });
@@ -48,10 +48,14 @@ document.addEventListener('DOMContentLoaded', () => {
           valueWrap.appendChild(copyIcon);
           wrapper.appendChild(valueWrap);
         });
+
+        // Append the wrapper after processing all sections
         main.appendChild(wrapper);
       });
-    })    
+      waitText.style.display = 'none';
+    })
     .catch(error => {
+      waitText.style.display = 'none';
       alert('Error fetching JSON data:', error);
     });
 });
